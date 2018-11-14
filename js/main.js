ec503b84866635014a54fb22214aa5776907daa0
jessicaseto <jes.seto@gmail.com>
2018-11-14 15:30:47 -0800

    Making alt text for images more descriptive

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.name;
+  image.alt = 'An image of ' + restaurant.name + ' restaurant in ' + restaurant.neighborhood;
   li.append(image);
 
   const name = document.createElement('h1');
